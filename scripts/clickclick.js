a02bb1b2b849d06eb9e125226ba881726879809c
miruskitsa <miranda.paz.st@gmail.com>
2025-06-16 07:23:46 -0300

    ahora si hasta 100 con mensaje de reinicio

--- scripts/clickclick.js
+++ scripts/clickclick.js
@@ -4,7 +4,20 @@ window.addEventListener('DOMContentLoaded', () => {
   const texto = document.getElementById('contador');
 
   boton.addEventListener('click', () => {
+    if (contador >= 100) {
+      const reiniciar = confirm("¿Pensaste que era infinito? Pues no. Mejor empeza de vuelta");
+      if (reiniciar) {
+        contador = 0;
+        texto.textContent = `Clics: ${contador}`;
+        return;
+      } else {
+        boton.disabled = true;
+        return;
+      }
+    }
+
     contador++;
     texto.textContent = `Clics: ${contador}`;
   });
 });
+
