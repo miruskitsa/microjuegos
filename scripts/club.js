c53d660682b2b7b5c8c3320e0c9f27719ee8bab1
miruskitsa <miranda.paz.st@gmail.com>
2025-06-16 06:54:57 -0300

    agregando la condicion de la ropa

--- scripts/club.js
+++ scripts/club.js
@@ -6,7 +6,7 @@ function iniciarSimulador() {
   const edad = parseInt(prompt("¿Cuántos años tenés?"));
 
   if (!nombre || isNaN(edad)) {
-    alert("Datos inválidos. No podés entrar.");
+    alert("Datos inválidos. No podés entrar. Al pato no se le miente eh...");
     console.log("Entrada cancelada por datos inválidos.");
     return;
   }
@@ -14,7 +14,22 @@ function iniciarSimulador() {
   console.log(`Usuario: ${nombre}, Edad: ${edad}`);
 
   if (edad < 18) {
-    alert("😬 Sos menor de edad. ¡No podés pasar!");
+    alert("😬 Sos menor de edad. ¡No pasas y te vas a ver pocoyo!");
     return;
   }
 }
+const ropa = prompt("¿Qué estás usando para salir?");
+  const permitidas = ["camisa", "remera", "vestido", "pantalón", "jean", "tanga", "short"];
+  let aprobado = false;
+
+  for (let prenda of permitidas) {
+    if (ropa.toLowerCase().includes(prenda)) {
+      aprobado = true;
+      break;
+    }
+  }
+
+  if (!aprobado) {
+    alert( "El patovica no te deja pasar con esa ropa... cambiate y proba de vuelta");
+    return;
+  }
