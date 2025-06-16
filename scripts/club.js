a05485205c9c1aab39ae061b1192d44de21989d7
miruskitsa <miranda.paz.st@gmail.com>
2025-06-16 06:41:38 -0300

    nombre y edad if y else js

---
+++ scripts/club.js
@@ -0,0 +1,19 @@
+document.getElementById('boton-club').addEventListener('click', iniciarSimulador);
+
+function iniciarSimulador() {
+  alert("Estás por intentar entrar al boliche...");
+  const nombre = prompt("¿Cómo te llamás?");
+  const edad = parseInt(prompt("¿Cuántos años tenés?"));
+
+  if (!nombre || isNaN(edad)) {
+    alert("Datos inválidos. No podés entrar.");
+    console.log("Entrada cancelada por datos inválidos.");
+    return;
+  }
+
+  console.log(`Usuario: ${nombre}, Edad: ${edad}`);
+
+  if (edad < 18) {
+    alert("😬 Sos menor de edad. ¡No podés pasar!");
+    return;
+  }
